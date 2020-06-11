2e5011b222448697eb654dce795e1e48501d890c
Mariannkmb <62664437+Mariannkmb@users.noreply.github.com>
2020-06-10 21:26:43 -0500

    Installing LoaderSpinner

--- src/LoaderSpinner.js
+++ src/LoaderSpinner.js
@@ -5,9 +5,9 @@ export default function LoaderSpinner() {
   return (
     <Loader
       type="Puff"
-      color="#EB5D9A"
-      height={50}
-      width={50}
+      color="#EC6E4C"
+      height={100}
+      width={100}
       timeout={3000}
       className="Loader"
     />
